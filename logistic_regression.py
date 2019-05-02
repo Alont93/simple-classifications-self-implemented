b1a328f18ea26b4969b02908da3d83abae7ade18
Alon Tron <alontron@cs.huji.ac.il>
2019-05-02 19:23:07 +0300

    7a done

--- logistic_regression.py
+++ logistic_regression.py
@@ -45,40 +45,44 @@ def check_Ni(test_set_lables, sorted_probabilities_indices, requested_TPR):
 
 def q7():
     data = read_data()
+
+    for i in range(1,10):
+        test_iteration(data, i)
+
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.title("ROC curve")
+    plt.legend()
+    plt.show()
+
+
+def test_iteration(data, test_number):
     train_set, test_set = randomly_split_data(data)
     train_set_samples, train_set_labels = split_samples_and_lables(train_set)
     test_set_samples, test_set_labels = split_samples_and_lables(test_set)
 
     clf = LogisticRegression()
-    clf.fit(train_set, train_set_labels)
-    predicted_probabilities = clf.predict_proba(test_set)[:,1]
+    clf.fit(train_set_samples, train_set_labels)
+
+    predicted_probabilities = clf.predict_proba(test_set_samples)[:, 1]
     sorted_probabilities_indices = np.argsort(predicted_probabilities)[::-1]
 
     NP = np.count_nonzero(test_set_labels == 1)
     NN = test_set_labels.size - NP
+    Nis = [0]
 
-    ROC_points = [(0,0), (1,1)]
-
-
-    Nis = []
-
-    for i in range(1000):
-        if(test_set_labels[sorted_probabilities_indices[i]] == 1):
+    for i in range(TEST_SET_AMOUNT):
+        if test_set_labels[sorted_probabilities_indices[i]] == 1:
             Nis.append(i)
 
-    # for i in range(NP):
-    #     TPR = i / NP
-    #     Ni = check_Ni(test_set_labels, sorted_probabilities_indices, TPR)
-    #     FPR = (Ni - i) / NN
-    #
-    #     ROC_points.append((TPR, FPR))
+    TPR = np.arange(NP + 1) / NP
+    Nis = np.array(Nis)
+    FPR = (Nis - np.arange(NP + 1)) / NN
 
-    plt.plot(ROC_points)
-    plt.show()
+    TPR = np.hstack((TPR, 1))
+    FPR = np.hstack((FPR, 1))
 
-
-
-
+    plt.plot(FPR, TPR, label="test " + str(test_number))
 
 
 q7()
