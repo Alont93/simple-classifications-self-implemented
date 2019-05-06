bcab41948de9c5e979642c0d5009d462465bb7e4
Alon Tron <alontron@cs.huji.ac.il>
2019-05-06 20:29:53 +0300

    ad chan

---
+++ QDA.py
@@ -0,0 +1,56 @@
+import numpy as np
+from functools import reduce
+
+
+class QDA:
+
+    def __init__(self):
+        self.__frequencies = None
+        self.__means = None
+        self.__covariance_matrices = None
+        self.__num_of_labels = None
+        self.__labels = None
+        self.__number_of_features = None
+
+    def fit(self, X, y):
+        number_of_samples = y.size
+        number_of_features = X.shape[1]
+        self.__number_of_features = number_of_features
+
+        unique, counts = np.unique(y, return_counts=True)
+        self.__labels = unique
+        self.__frequencies = counts / number_of_samples
+
+        self.__num_of_labels = unique.size
+        self.__means = np.zeros((self.__num_of_labels, number_of_features))
+
+        sorted_indices = np.argsort(y)
+        X_sorted_by_labels = X[sorted_indices]
+        X_splited_by_labels = np.array_split(X_sorted_by_labels, np.where(np.diff(y[sorted_indices]) != 0)[0] + 1)
+
+        for i in range(self.__num_of_labels):
+            self.__means[i] = np.sum(X_splited_by_labels[i], axis=0) / counts[i]
+
+        self.__covariance_matrices = np.zeros((self.__num_of_labels, self.__number_of_features,
+                                               self.__number_of_features))
+        for i in range(self.__num_of_labels):
+            X_minus_mean = X_splited_by_labels[i]
+            self.__covariance_matrices[i] = X_minus_mean.T @ X_minus_mean
+
+    def predict(self, x):
+        sigmas_inv = np.zeros_like(self.__covariance_matrices)
+        for i in range(self.__num_of_labels):
+            sigmas_inv[i] = np.linalg.inv(self.__covariance_matrices[i])
+
+
+        xT = x.reshape((1, self.__number_of_features))
+        means = self.__means
+
+        probabilities = np.zeros_like(self.__labels)
+        for i in range(self.__num_of_labels):
+            label_mean = self.__means[i].reshape(self.__number_of_features, 1)
+            probabilities[i] = xT @ sigmas_inv[i] @ label_mean \
+                               - 0.5 * label_mean.T @ sigmas_inv[i] @ label_mean + np.log(self.__frequencies[i])
+
+        max_probability_index = np.argmax(probabilities)
+        return self.__labels[max_probability_index]
--- test_discriminant_analysis.py
+++ test_discriminant_analysis.py
@@ -4,8 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import LDA
+import QDA
 
-NUMBER_OF_TESTS = 10
+NUMBER_OF_TESTS = 3
 TEST_SET_AMOUNT = 1000
 DATA_PATH = './spam.data.txt'
 
@@ -48,9 +49,9 @@ def test_iteration(data, test_number):
     train_set_samples, train_set_labels = split_samples_and_lables(train_set)
     test_set_samples, test_set_labels = split_samples_and_lables(test_set)
 
-    lda_learner = LDA.LDA()
-    lda_learner.fit(train_set_samples, train_set_labels)
-    predictions = np.apply_along_axis(lda_learner.predict, 1, test_set_samples)
+    da_learner = QDA.QDA()
+    da_learner.fit(train_set_samples, train_set_labels)
+    predictions = np.apply_along_axis(da_learner.predict, 1, test_set_samples)
 
     error = np.sum(np.square(predictions - test_set_labels)) / TEST_SET_AMOUNT
     return error
